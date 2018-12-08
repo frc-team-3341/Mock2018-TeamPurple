b74a93b86c946cf6b2a2ce973ed9e38d5a3a30b3
Dave Melkani <davemelkani@gmail.com>
2018-12-08 14:59:26 -0800

    servo code

---
+++ src/main/cpp/Subsystems/ServoMech.cpp
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+#include <Subsystems/ServoMech.h>
+#include "WPILib.h"
+#include "../RobotMap.h"
+
+ServoMech::ServoMech() : Subsystem("Servo"), purpleServo(new Servo(0)){
+
+}
+
+ServoMech::~ServoMech()
+{
+	delete purpleServo;
+	purpleServo = nullptr;
+}
+
+void ServoMech::InitDefaultCommand() {
+
+	// Set the default command for a subsystem here.
+	// SetDefaultCommand(new MySpecialCommand());
+}
+
+void ServoMech::move(double angle){
+	purpleServo->Set(angle);
+}
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
